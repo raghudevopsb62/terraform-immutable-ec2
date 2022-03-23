8976de66490613547c659703dc1521eb084a372b
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-03-23 08:18:59 +0530

    Update terraform-modules/terraform-immutable-ec2/

--- ec2.tf
+++ ec2.tf
@@ -4,21 +4,7 @@ resource "aws_instance" "od-instance" {
   ami                    = data.aws_ami.ami.id
   subnet_id              = element(data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNETS_IDS, count.index)
   vpc_security_group_ids = [aws_security_group.allow.id]
-  user_data              = <<EOF
-#!/bin/bash
-
-if [ -f /etc/nginx/default.d/roboshop.conf ]; then
-  sed -i -e 's/ENV/${var.ENV}/' /etc/nginx/default.d/roboshop.conf
-  systemctl restart nginx
-  exit
-fi
-
-COMPONENT=$(ls /home/roboshop/)
-sed -i -e 's/ENV/${var.ENV}/' /etc/systemd/system/\${COMPONENT}.service
-systemctl daemon-reload
-systemctl restart \${COMPONENT}
-
-EOF
+  user_data              = file("${var.ENV}-userdata.sh")
 }
 
 resource "aws_spot_instance_request" "spot-instance" {
@@ -28,19 +14,5 @@ resource "aws_spot_instance_request" "spot-instance" {
   wait_for_fulfillment   = true
   subnet_id              = element(data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNETS_IDS, count.index + 1)
   vpc_security_group_ids = [aws_security_group.allow.id]
-  user_data              = <<EOF
-#!/bin/bash
-
-if [ -f /etc/nginx/default.d/roboshop.conf ]; then
-  sed -i -e 's/ENV/${var.ENV}/' /etc/nginx/default.d/roboshop.conf
-  systemctl restart nginx
-  exit
-fi
-
-COMPONENT=$(ls /home/roboshop/)
-sed -i -e 's/ENV/${var.ENV}/' /etc/systemd/system/\${COMPONENT}.service
-systemctl daemon-reload
-systemctl restart \${COMPONENT}
-
-EOF
+  user_data              = file("${var.ENV}-userdata.sh")
 }
