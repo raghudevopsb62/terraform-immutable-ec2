21a6b8276944f8270bb387a45931dac26096c17c
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-03-23 07:59:05 +0530

    Update terraform-modules/terraform-immutable-ec2/

--- data.tf
+++ data.tf
@@ -1,6 +1,6 @@
 data "aws_ami" "ami" {
   most_recent = true
-  name_regex  = "base-with-ansible"
+  name_regex  = "${var.COMPONENT}-${var.APP_VERSION}"
   owners      = ["self"]
 }
 
--- ec2.tf
+++ ec2.tf
@@ -1,14 +1,14 @@
 resource "aws_instance" "od-instance" {
   count                  = var.OD_INSTANCE_COUNT
   instance_type          = var.OD_INSTANCE_TYPE
-  ami                    = "${var.COMPONENT}-${var.APP_VERSION}"
+  ami                    = data.aws_ami.ami.id
   subnet_id              = element(data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNETS_IDS, count.index)
   vpc_security_group_ids = [aws_security_group.allow.id]
 }
 
 resource "aws_spot_instance_request" "spot-instance" {
   count                  = var.SPOT_INSTANCE_COUNT
-  ami                    = "${var.COMPONENT}-${var.APP_VERSION}"
+  ami                    = data.aws_ami.ami.id
   instance_type          = var.SPOT_INSTANCE_TYPE
   wait_for_fulfillment   = true
   subnet_id              = element(data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNETS_IDS, count.index + 1)
