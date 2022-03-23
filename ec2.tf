dae68e4bbab583e1b82f72c383fb06d5100252fa
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-03-23 07:39:20 +0530

    Update terraform-modules/terraform-immutable-ec2/

--- ec2.tf
+++ ec2.tf
@@ -14,23 +14,3 @@ resource "aws_spot_instance_request" "spot-instance" {
   subnet_id              = element(data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNETS_IDS, count.index + 1)
   vpc_security_group_ids = [aws_security_group.allow.id]
 }
-
-resource "null_resource" "ansible-apply" {
-
-  triggers = {
-    TRIGGER     = var.TRIGGER
-    APP_VERSION = var.APP_VERSION
-  }
-
-  count = length(local.ALL_INSTANCE_IPS)
-  provisioner "remote-exec" {
-    connection {
-      host     = element(local.ALL_INSTANCE_IPS, count.index)
-      user     = jsondecode(data.aws_secretsmanager_secret_version.latest.secret_string)["SSH_USER"]
-      password = jsondecode(data.aws_secretsmanager_secret_version.latest.secret_string)["SSH_PASS"]
-    }
-    inline = [
-      "ansible-pull -U https://github.com/raghudevopsb62/ansible roboshop-pull.yml -e COMPONENT=${var.COMPONENT} -e ENV=${var.ENV} -e APP_VERSION=${var.APP_VERSION}"
-    ]
-  }
-}
