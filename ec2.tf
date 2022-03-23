84a01056f5d62908d9bb4cdb7c7780ffbda1a83c
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-03-23 08:14:49 +0530

    Update terraform-modules/terraform-immutable-ec2/

--- ec2.tf
+++ ec2.tf
@@ -28,4 +28,19 @@ resource "aws_spot_instance_request" "spot-instance" {
   wait_for_fulfillment   = true
   subnet_id              = element(data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNETS_IDS, count.index + 1)
   vpc_security_group_ids = [aws_security_group.allow.id]
+  user_data              = <<EOF
+#!/bin/bash
+
+if [ -f /etc/nginx/default.d/roboshop.conf ]; then
+  sed -i -e 's/ENV/${var.ENV}/' /etc/nginx/default.d/roboshop.conf
+  systemctl restart nginx
+  exit
+fi
+
+COMPONENT=$(ls /home/roboshop/)
+sed -i -e 's/ENV/${var.ENV}/' /etc/systemd/system/${COMPONENT}.service
+systemctl daemon-reload
+systemctl restart ${COMPONENT}
+
+EOF
 }
