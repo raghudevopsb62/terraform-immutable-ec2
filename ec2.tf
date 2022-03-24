768522de63911a791deeab1ddb9002e3f1ab0a8a
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-03-24 08:24:36 +0530

    Update terraform-modules/terraform-immutable-ec2/

--- ec2.tf
+++ ec2.tf
@@ -48,6 +48,7 @@ resource "aws_autoscaling_group" "asg" {
   desired_capacity    = var.ASG_DESIRED
   max_size            = var.ASG_MAX
   min_size            = var.ASG_MIN
+  target_group_arns   = [aws_lb_target_group.tg.arn]
 
   launch_template {
     id      = aws_launch_template.template.id
