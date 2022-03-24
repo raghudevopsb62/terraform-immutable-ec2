7997db32b6c52804d562ff047e03e4a92706c042
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-03-24 08:20:54 +0530

    Update terraform-modules/terraform-immutable-ec2/

--- alb.tf
+++ alb.tf
@@ -1,17 +1,17 @@
-//resource "aws_lb_target_group" "tg" {
-//  name     = "${var.COMPONENT}-${var.ENV}"
-//  port     = var.PORT
-//  protocol = "HTTP"
-//  vpc_id   = data.terraform_remote_state.vpc.outputs.VPC_ID
-//  health_check {
-//    enabled             = true
-//    path                = "/health"
-//    unhealthy_threshold = 2
-//    healthy_threshold   = 2
-//    interval            = 5
-//    timeout             = 2
-//  }
-//}
+resource "aws_lb_target_group" "tg" {
+  name     = "${var.COMPONENT}-${var.ENV}"
+  port     = var.PORT
+  protocol = "HTTP"
+  vpc_id   = data.terraform_remote_state.vpc.outputs.VPC_ID
+  health_check {
+    enabled             = true
+    path                = "/health"
+    unhealthy_threshold = 2
+    healthy_threshold   = 2
+    interval            = 5
+    timeout             = 2
+  }
+}
 //
 //resource "aws_lb_target_group_attachment" "tg-attach" {
 //  count            = length(local.ALL_INSTANCE_IDS)
