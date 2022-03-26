c4118fe1030c1415362f087f401930e5edd21e14
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-03-26 07:37:55 +0530

    Update terraform-modules/terraform-immutable-ec2/

--- dev-userdata.sh
+++ dev-userdata.sh
@@ -3,6 +3,7 @@
 if [ -f /etc/nginx/default.d/roboshop.conf ]; then
   sed -i -e 's/ENV/dev/' /etc/nginx/default.d/roboshop.conf /etc/filebeat/filebeat.yml
   systemctl restart nginx
+  systemctl restart filebeat
   exit
 fi
 
@@ -10,3 +11,4 @@ COMPONENT=$(ls /home/roboshop/)
 sed -i -e 's/ENV/dev/' /etc/systemd/system/${COMPONENT}.service /etc/filebeat/filebeat.yml
 systemctl daemon-reload
 systemctl restart ${COMPONENT}
+systemctl restart filebeat
